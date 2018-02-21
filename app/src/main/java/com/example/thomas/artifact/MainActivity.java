860773b4ba696bad8f4943813ccbc952af820998
Thomas Whitelock <trnthomas@live.com>
2018-02-20 20:41:47 -0500

    Just a test, per the assignment.

--- app/src/main/java/com/example/thomas/artifact/MainActivity.java
+++ app/src/main/java/com/example/thomas/artifact/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.thomas.artifact;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+// thomas was here
+// and again  ....
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int i;
+        //gggdgf
     }
 
     // just a test - Pye
