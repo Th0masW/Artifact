8167d103351fff050126eecafe3875bc77578d84
Furry Mammal <caseypye@hotmail.com>
2018-03-13 18:35:36 -0600

    Team activity- log

--- app/src/main/java/com/example/thomas/artifact/MainActivity.java
+++ app/src/main/java/com/example/thomas/artifact/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.thomas.artifact;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 // thomas was here
 // and again  ....
@@ -15,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         int i;
+        doSomething();
 
     }
 
@@ -23,6 +27,16 @@ public class MainActivity extends AppCompatActivity {
     }
     public void openReviewEvidence(View view) {
         startActivity(new Intent(MainActivity.this, ReviewEvidence.class));
+    }
+
+    private void doSomething() {
+        String tag = "Do Something";
+        String msg = "Say something";
+        Log.e(tag, msg);
+        Log.v(tag, "This is a verbose log");
+        Log.wtf(tag, "WTF!");
+
+
     }
 
 }
